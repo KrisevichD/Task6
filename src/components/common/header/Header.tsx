6ae20d8155777acbb5f2bdfd1ae2550f972f65dd
KrisevichD <denis.krisevich@gmail.com>
2026-07-07 21:38:23 +0300

    feat: responsive nav menu

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,29 +1,20 @@
 import { Icon } from "@iconify/react";
-import { Link } from "@tanstack/react-router";
 import { Button } from "../../ui/button";
+import NavMenu from "./NavMenu";
 import ProfileButton from "./ProfileButton";
 import ThemeButton from "./ThemeButton";
 
 const Header = () => {
 
     return (
-        <header className="h-16 w-screen bg-card px-3 py-6 flex justify-between">
-            <nav className="hidden sm:flex w-full gap-10.5 text-sm font-medium justify-center items-center">
-                <Link to={'/'}>Home</Link>
-                <Link to={'/dashboard'}>Tables</Link>
-                <Link to={'/tables'}>Process</Link>
-                <Link to={'/'}>Documentation</Link>
-            </nav>
-            <nav className="block sm:hidden">
-                ...
-            </nav>
+        <header className="h-16 w-screen bg-card px-3 py-6 flex justify-between items-center">
+            <NavMenu />
             <div className="flex gap-3 items-center">
                 <ThemeButton />
                 <Button variant={'ghost'} size={'icon-md'}>
-                    <Icon icon={'streamline:interface-alert-alarm-bell-2-alert-bell-ring-notification-alarm'}/>
+                    <Icon icon={'streamline:interface-alert-alarm-bell-2-alert-bell-ring-notification-alarm'} />
                 </Button>
                 <ProfileButton />
-
             </div>
         </header>
     );
---
+++ src/components/common/header/NavMenu.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Icon } from "@iconify/react";
+import { Link } from "@tanstack/react-router";
+
+
+const NavMenu = () => {
+    return (
+        <>
+            <div className="block sm:hidden">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant={'ghost'} size={'icon-lg'}>
+                            <Icon icon={'fontisto:nav-icon-a'} />
+                            <span className="sr-only">Navigation menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-full">
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem asChild>
+                                <Link to="/">Home</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem asChild>
+                                <Link to="/dashboard">Tables</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem asChild>
+                                <Link to="/tables">Process</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem asChild>
+                                <Link to="/">Documentation</Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+            <nav className="hidden sm:flex w-full gap-10.5 text-sm font-medium justify-center items-center">
+                <Link to="/">Home</Link>
+                <Link to="/dashboard">Tables</Link>
+                <Link to="/tables">Process</Link>
+                <Link to="/">Documentation</Link>
+            </nav>
+        </>
+    );
+}
+
+export default NavMenu;
