66b3c317877e1ddee16ae10da7b5b8c70fb0f65e
KrisevichD <denis.krisevich@gmail.com>
2026-07-08 21:08:40 +0300

    fix: h2 style

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -7,7 +7,7 @@ const Title = ({ title, subtitle }: TitleProps) => {
     return (
         <div className="pt-2 pb-5.25">
             <h1>{title}</h1>
-            {subtitle && <h2 className="mt-2 text-sm font-medium">{subtitle}</h2>}
+            {subtitle && <h2 className="mt-2 text-sm text-foreground font-medium">{subtitle}</h2>}
         </div>
     );
 }
